d6fd7c86c654903135048f0b760077e4f5e23d55
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-02-06 17:31:50 +0530

    updated

--- 01_Basics/Basics.js
+++
@@ -1,7 +0,0 @@
-console.log("Today i am learning Node js");
-//To run this Node js file You need Command ==> Node Basics.js
-
-
-// console.log(window);
-
-// This is not Available in Node .it can run in Browser same for alert
