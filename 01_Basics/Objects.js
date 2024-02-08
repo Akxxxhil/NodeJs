3acde3f250ab79f158071079dbc2f482260e8fc3
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-02-08 18:45:43 +0530

    updated

---
+++ 01_Basics/Objects.js
@@ -0,0 +1,12 @@
+var person={
+    name:"Akxxhil",
+    age:19,
+    hobby:"Video games"
+}
+
+console.log(person);
+
+// Here the person is the object. And the name ,age,Hobby are the key and the right side's are the values .
+
+//the key can only be the string /Symbol
+//while the values can be any data type.
